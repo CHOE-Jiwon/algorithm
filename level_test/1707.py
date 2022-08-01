1a8180ecf4cc2ad601a10982299a60a000be6b01
최지원 <jiwon0929@crowdworks.kr>
2022-08-01 22:29:37 +0900

    1707 푸는거 저장

--- level_test/1707.py
+++ level_test/1707.py
@@ -1,20 +1,30 @@
-# 작성일: 2022-07-06
-# 문제 링크: https://www.acmicpc.net/problem/1874
+# 작성일: 2022-08-01
+# 문제 링크: https://www.acmicpc.net/problem/1707
+# 그래프를 만들고 -> 첫번째 노드부터 BFS든 DFS든 해보자.
 ##############################################
 import sys
-
+from collections import deque
 
 testCnt = int(sys.stdin.readline())
 
 for _ in range(testCnt):
     v_cnt, e_cnt = map(int, sys.stdin.readline().split())
 
-    graphs = list()
+    graphs = dict()
 
+    # graph 생성
     for __ in range(e_cnt):
         u, v = map(int, sys.stdin.readline().split())
 
-        graphs.append([u,v])
+        if u not in graphs:
+            graphs[u] = [v]
+        else:
+            graphs[u].append(v)
+        
+        if v not in graphs:
+            graphs[v] = [u]
+        else:
+            graphs[v].append(u)
     
-    for x in sorted(graphs, key = lambda x:min(x)):
-        print(min(x),max(x))
+    queue = deque()
+    for k, v in graphs.items():
